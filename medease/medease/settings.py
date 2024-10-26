633c9e4c49a3f25c0be88417d097e3c783b29a36
ijuz <ijazijuz66@gmail.com>
2024-10-26 21:26:21 +0530

    'test

--- medease/medease/settings.py
+++ medease/medease/settings.py
@@ -42,7 +42,7 @@ ROOT_URLCONF = 'medease.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -110,6 +110,9 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
